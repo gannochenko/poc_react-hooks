1ee2259f85be506055dfcc740c5e4c1ff73f218a
Sergei <gannochenko.sv@gmail.com>
2018-12-30 19:47:42 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+const SubComponent = () => {
+    console.dir('SubComponent render!');
+
+    const [forBatman, setForBatman] = useState(0);
+    const [forJoker, setForJoker] = useState(0);
+    useEffect(() => {
+        let title = 'Who will prevail?';
+        if (forBatman > forJoker) {
+            title = 'Good is winning';
+        } else if (forJoker > forBatman) {
+            title = 'Evil is upon us!'
+        }
+        document.title = title;
+
+        console.dir('Effect called!');
+
+        return () => {
+            console.dir('SubComponent unmounted');
+            document.title = 'The fight is over';
+        };
+    }, [forBatman, forJoker]);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <React.Fragment>
+            Batman: {forBatman}
+            {' '}vs
+            Joker: {forJoker}
+            <button
+                onClick={() => setForBatman(forBatman + 1)}
+                style={{padding: '1rem 2rem'}}
+            >
+                One for Batman!
+            </button>
+            <button
+                onClick={() => setForJoker(forJoker + 1)}
+                style={{padding: '1rem 2rem'}}
+            >
+                One for Joker!
+            </button>
+        </React.Fragment>
     );
-  }
-}
+};
+
+const App = () => {
+    console.dir('App render!');
+
+    const [appCounter, setAppCounter] = useState(0);
+    return (
+        <div className="App">
+            <header className="App-header">
+                {
+                    appCounter < 5
+                    &&
+                    <SubComponent/>
+                }
+                <br />
+                Application counter: {appCounter}
+                <button
+                    onClick={() => setAppCounter(appCounter + 1)}
+                    style={{padding: '1rem 2rem'}}
+                >
+                    One for application
+                </button>
+            </header>
+        </div>
+    );
+};
 
 export default App;
